dd37ff894759387f4dd74e7e2dc6712490fc8b4c
Tejendra Pal Singh <110801292+tejth@users.noreply.github.com>
2023-09-07 15:17:25 +0530

    Add files via upload

---
+++ Strings/RomanToInteger.java
@@ -0,0 +1,47 @@
+public class RomanToInteger {
+    public int romanToInt(String s) {
+          int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = 0;
+
+            switch (currentChar) {
+                case 'I':
+                    currentValue = 1;
+                    break;
+                case 'V':
+                    currentValue = 5;
+                    break;
+                case 'X':
+                    currentValue = 10;
+                    break;
+                case 'L':
+                    currentValue = 50;
+                    break;
+                case 'C':
+                    currentValue = 100;
+                    break;
+                case 'D':
+                    currentValue = 500;
+                    break;
+                case 'M':
+                    currentValue = 1000;
+                    break;
+                default:
+                    return -1; // Invalid Roman numeral
+            }
+
+            if (prevValue > currentValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
