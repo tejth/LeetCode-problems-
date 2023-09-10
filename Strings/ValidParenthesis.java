fa68d517f69ffe2e46e649a0f7bb907d9b823871
Tejendra Pal Singh <110801292+tejth@users.noreply.github.com>
2023-09-10 16:42:00 +0530

    Add files via upload

---
+++ Strings/ValidParenthesis.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "()";
+        boolean result = isValid(input);
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + result);
+    }
+}
